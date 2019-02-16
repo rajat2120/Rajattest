9708ec0d869262f9fcc356294aa09b96c918d61e
rajat2120 <rajatbanga1017@gmail.com>
2019-02-16 22:40:13 +0100

    adding

--- RajatTesting/src/test/HelloWorld.java
+++ RajatTesting/src/test/HelloWorld.java
@@ -1,6 +1,7 @@
 package test;
 
 public class HelloWorld {
+	//This comment is added in rajatbranch
 
 	public static void main (String Args []) {
 		
---
+++ RajatTesting/src/test/New1.java
@@ -0,0 +1,15 @@
+package test;
+
+public class New1 {
+	int age ;
+	String Name ;
+	
+	//Creating a object for this class 
+	
+	public static void main (String args []  ){
+		New1 Rajat = new New1 ();	
+
+		Rajat.age =29;
+			System.out.println ("Age of Rajat is "+ Rajat.age);
+	}
+}
